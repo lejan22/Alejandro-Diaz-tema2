8935448f0cf7b829b10f38cb9b25fba9d3ada93d
lejan22 <alejandrodl2206@gmail.com>
2021-10-26 12:55:26 +0200

    26octubre

--- tema2/Assets/scripts/MyFirstScript.cs
+++ tema2/Assets/scripts/MyFirstScript.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class MyFirstScript : MonoBehaviour
 {
-
+    public int number1;
+    public int number2;
 
     private float playerSpeed = 5.25f;
     public string playerName = "Lejan";
@@ -27,12 +28,15 @@ public class MyFirstScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log(Product(number1, number2));
 
-        HelloWorld();
-        hello = GetHello();
-        Debug.Log(hello);
 
-       // Debug.Log( GetHello())
+        //HelloWorld();
+        //hello = GetHello();
+        //Debug.Log(hello);
+
+
+        // Debug.Log( GetHello())
 
         //transform.position = new Vector3(0,0,0);
         transform.position = Vector3.zero;
@@ -80,51 +84,27 @@ public class MyFirstScript : MonoBehaviour
     {
         // Debug.Log(transform.position);
         //movimiento personaje
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            transform.position += Vector3.right;
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            transform.position += Vector3.left;
-        }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            transform.position += Vector3.forward;
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            transform.position += Vector3.back;
-        }
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            transform.position += Vector3.up;
-        }
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            transform.position += Vector3.down;
-        }
+        MovementToDirection(KeyCode.D, Vector3.right);
+        MovementToDirection(KeyCode.A, Vector3.left);
+        MovementToDirection(KeyCode.W, Vector3.forward);
+        MovementToDirection(KeyCode.S, Vector3.back);
+        MovementToDirection(KeyCode.E, Vector3.up);
+        MovementToDirection(KeyCode.Q, Vector3.down);
+
         //Rotacion personaje
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            transform.rotation *= Quaternion.Euler(0, 10, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            transform.rotation *= Quaternion.Euler(0, -10, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            transform.rotation *= Quaternion.Euler(-10, 0, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            transform.rotation *= Quaternion.Euler(10, 0, 0);
-        }
+        RotationToDirection(KeyCode.RightArrow, new Vector3(0, 10, 0));
+        RotationToDirection(KeyCode.LeftArrow, new Vector3(0, -10, 0));
+        RotationToDirection(KeyCode.UpArrow, new Vector3(10, 0, 0));
+        RotationToDirection(KeyCode.DownArrow, new Vector3(-10, 0, 0));
+
+        //Escalar
         if (Input.GetKeyDown(KeyCode.R))
         {
             transform.localScale += Vector3.one;
         }
+        ScaleFigure(KeyCode.X, Vector3.right);
+        ScaleFigure(KeyCode.Y, Vector3.up);
+        ScaleFigure(KeyCode.U, Vector3.forward);
     }
 
     public void HelloWorld()
@@ -137,4 +117,36 @@ public class MyFirstScript : MonoBehaviour
         return "Hola";
     }
 
+    public void MovementToDirection(KeyCode key, Vector3 direction)
+    {
+        if (Input.GetKeyDown(key))
+        {
+            transform.position += direction;
+        }
+
+
+    }
+
+    public void ScaleFigure(KeyCode key, Vector3 axis)
+    {
+        if (Input.GetKeyDown(key))
+        {
+            transform.localScale += axis;
+        }
+
+    }
+    public void RotationToDirection(KeyCode key, Vector3 axis)
+    {
+        if (Input.GetKeyDown(key))
+        {
+            transform.rotation *= Quaternion.Euler(axis);
+        }
+    }
+    public int Product(int num1, int num2)
+    {
+        int result = num1 * num2;
+        Debug.Log($"{num1} * {num2} = {result}");
+        return result;
+    }
 }
+
